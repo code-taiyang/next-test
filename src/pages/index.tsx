75b747a2580173352078d083c39c936770b13e95
shenyang <15586252890@163.com>
2023-05-22 19:59:52 +0800

    next.js 学习

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import Layout from "@/components/layout";
-import Image from "next/image";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
-export default function Home(){
-    return (
-        <Layout title={"next应用主页"}>
-            <h1>Hello Next.js</h1>
-            <Image src="/image/四谎.jpg" alt="四月是你的谎言" width={1080 / 5} height={956 / 5} ></Image>
-        </Layout>
-    )
+export default function Home() {
+  return (
+    <Layout home>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
 }
